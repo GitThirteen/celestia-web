8f7715aa3b707b2282078a4444e52c4907ea0a25
bitThirteen <mich1.eickmeyer@gmail.com>
2021-09-24 23:48:06 +0200

    Completely insane text formatting

--- frontend/src/app/components/faq/faq.component.ts
+++ frontend/src/app/components/faq/faq.component.ts
@@ -8,6 +8,13 @@ type Question = {
   answer: string
 };
 
+type AltString = {
+  magic: string;
+  first: string;
+  format: string;
+  second: string;
+};
+
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
@@ -16,9 +23,9 @@ type Question = {
 export class FaqComponent implements OnInit {
   categories = ['GENERAL', 'GAMEPLAY', 'DONATIONS', 'MISC'];
   questions: Map<string, Question[]>;
+  counter = 0;
 
   private elLength = 0;
-  private counter = 0;
 
   constructor(private titleService: Title) {
     this.questions = this.loadQuestions();
@@ -39,11 +46,40 @@ export class FaqComponent implements OnInit {
     return questions;
   }
 
+  format(text: string, index: number): string {
+    if (text.includes('$')) {
+      const fs = this.stringSplit(text);
+
+      switch (fs.magic) {
+        case 'code': {
+          const answer = document.getElementById('answer_' + index);
+          if (answer) { answer.innerHTML = `${fs.first} <span style="background-color: black">${fs.format}</span>${fs.second}`; }
+          break;
+        }
+        default: console.log('yeet');
+      }
+    }
+
+    return text;
+  }
+
   count(): number {
     if (this.counter >= this.elLength) {
       this.counter = 0;
     }
     this.counter++;
     return this.counter;
+  }
+
+  private stringSplit(text: string): AltString {
+    const firSeg = text.split('$');
+    const secSeg = (text.split('{').pop() as string).split('}');
+
+    return {
+      first: firSeg[0],
+      magic: firSeg[1],
+      format: secSeg[0],
+      second: secSeg[1]
+    };
   }
 }
